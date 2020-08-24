445e9eae29e8f79b5ca2649c55d79dcb6c572539
Christian L <therishling@gmail.com>
2020-08-24 18:55:21 -0400

    Proyecto Presupuesto

---
+++ presupuesto/src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import Pregunta from './components/Pregunta'
+
+function App() {
+
+  const [presupuesto, guardarPresupuesto] = useState(0);
+  const [restante, guardarRestante] = useState(0);
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Gasto Semanal</h1>
+
+        <div className="contenido-principal contenido">
+        <Pregunta 
+          guardarPresupuesto = {guardarPresupuesto}
+          guardarRestante = {guardarRestante}
+        />  
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
